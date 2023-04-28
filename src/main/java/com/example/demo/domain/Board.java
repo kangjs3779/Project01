e1ade7ec147f0293d5140f56d304a4cadf10e599
jisookkang <123609533+kangjs3779@users.noreply.github.com>
2023-04-29 00:33:03 +0900

    0429

--- src/main/java/com/example/demo/domain/Board.java
+++ src/main/java/com/example/demo/domain/Board.java
@@ -11,6 +11,42 @@ public class Board {
 	private String body;
 	private LocalDateTime inserted;
 	private String writer;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public LocalDateTime getInserted() {
+		return inserted;
+	}
+	public void setInserted(LocalDateTime inserted) {
+		this.inserted = inserted;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	@Override
+	public String toString() {
+		return "Board [id=" + id + ", title=" + title + ", body=" + body + ", inserted=" + inserted + ", writer="
+				+ writer + "]";
+	}
+	
 }
 
 
